74698f9e39449728b8b47a772b810d9387f1fa95
sauravsahu02 <sauravsahu02@gmail.com>
2021-05-19 00:42:46 +0530

    [Google-Problem] Contiguous Subarray Sum

--- src/main/scala/google/problems/ContiguousSubarraySumWithinRange.scala
+++ src/main/scala/google/problems/ContiguousSubarraySumWithinRange.scala
@@ -1,5 +1,7 @@
 package google.problems
 
+import scala.collection.mutable.TreeSet
+
 class ContiguousSubarraySumWithinRange(val inputArr: Array[Int]) extends SingleIntArrayOperation(inputArr){
 /*
  https://www.careercup.com/question?id=5200686994161664
@@ -16,10 +18,27 @@ class ContiguousSubarraySumWithinRange(val inputArr: Array[Int]) extends SingleI
  return result
 
  Examples:
- count([1,2,3], 0, 3) = 3 # [1], [2], [3], [1, 2], [3]
+ count([1,2,3], 0, 3) = 5 # [1], [2], [3], [1, 2], [3]
  count([-2,5,-1], -2, 2) = 3 # [-2], [-1], [-2, 5, -1]
  You may assume that there are no overflows, that is sum(|x_i|) <= MAX_INT - 1
  */
 
-  def getCount(lowerBound: Int, upperBound: Int): Int = ???
+  def getCount(lowerBound: Int, upperBound: Int): Int = {
+    // Algorithm Explained:
+    // Consider input Array(-2, 5, -1) and Range: [-2, 2]
+    // -2 | CumSum: -2 ; SelfInRange ; no candidate exists | TreeSet((-2,0))
+    // -2 5 | CumSum : 3 ; SelfNotInRange; CandidatesToBeInRange [3-2, 3-(-2)] : [1, 5] : no candidate found | TreeSet((-2,0), (3,1))
+    // -2 5 -1 | CumSum : 2 ; SelfInRange; CandidatesToBeInRange [2-2, 2+2] : [0, 4] : RightCandidate (3,1) | TreeSet((-2,0), (3,1), (2, 1))
+    // Ans is 3 = [-2 (SelfInRange), -1 (SelfInRange), (3,1) (RightCandidate)]
+    var cumSum = 0
+    var count = 0
+    val treeSet : TreeSet[(Int, Int)] = TreeSet.empty
+    treeSet.addOne((0, -1))
+    for (index <- 0 to inputArr.size-1){
+      cumSum += inputArr(index)
+      count += treeSet.range((cumSum - upperBound, -1), (cumSum-lowerBound+1, -1)).size
+      treeSet.addOne((cumSum, index))
+    }
+    count
+  }
 }
--- src/test/scala/google/problems/ContiguousSubarraySumWithinRangeTest.scala
+++ src/test/scala/google/problems/ContiguousSubarraySumWithinRangeTest.scala
@@ -6,10 +6,22 @@ class ContiguousSubarraySumWithinRangeTest extends FunSuite with BeforeAndAfter{
 
   test("Input 1") {
     val input = Array(1,2,3)
-    assert(new ContiguousSubarraySumWithinRange(input).getCount(0, 3) == 3)
+    assert(new ContiguousSubarraySumWithinRange(input).getCount(0, 3) == 4)
+    // 1, 2, 3, (1,2)
   }
   test("Input 2") {
     val input = Array(-2,5,-1)
     assert(new ContiguousSubarraySumWithinRange(input).getCount(-2, 2) == 3)
+    // (-2), (-1), (-2, 5, -1)
   }
+  test("Input 3") {
+    val input = Array(2,-1,2)
+    assert(new ContiguousSubarraySumWithinRange(input).getCount(1, 3) == 5)
+    // (2), (2, -1), (2), (-1, 2), (2, -1, 2)
+  }
+  test("Input 4") {
+    val input = Array(10, -10, 10, -10)
+    assert(new ContiguousSubarraySumWithinRange(input).getCount(-10, 10) == 10)
+  }
+
 }
