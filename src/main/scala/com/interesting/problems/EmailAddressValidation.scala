24199189f424282cbc6ed2c2e181c89ea0273408
sauravsahu02 <sauravsahu02@gmail.com>
2021-06-10 20:17:55 +0530

    Removing comments in email-address - disallowing comments in mid

--- src/main/scala/com/interesting/problems/EmailAddressValidation.scala
+++ src/main/scala/com/interesting/problems/EmailAddressValidation.scala
@@ -1,5 +1,3 @@
-package com.interesting.problems
-
 /**
  * Created by Saurav Sahu on 10th June 2021
  */
@@ -7,20 +5,28 @@ object EmailAddressValidation {
   // As poer Wiki-page, comments are allowed with parentheses at either end of the local-part;
   // e.g., john.smith(comment)@example.com and (comment)john.smith@example.com
   // are both equivalent to john.smith@example.com.
-  type localAndDomainType = Option[(/*local part*/ String, /*domain part*/ String)]
-  def getLocalPartAndDomain(emailAddress: String): localAndDomainType = {
+  def getLocalPartAndDomain(emailAddress: String): Option[(/*local part*/ String, /*domain part*/ String)] = {
     val localAndDomainPart = Array(new StringBuilder, new StringBuilder)
     var counter = 0
     var isDomain = 0
+    var index = 0
+    var openBracketPosition = 0
     emailAddress.toCharArray.foreach{
-        c: Char => c match {
-          case '(' => counter += 1
-          case ')' => counter -= 1
-          case '@' =>
-            if (counter != 0 || isDomain == 1) return None
-            isDomain = 1 - isDomain
-          case _  => if (counter == 0) localAndDomainPart(isDomain).append(c)
-        }
+      c: Char => c match {
+        case '(' =>
+          counter += 1
+          if (counter == 1) openBracketPosition = index
+        case ')' =>
+          counter -= 1
+          if (index != emailAddress.indexOf('@') - 1 && openBracketPosition != 0) {
+            return None
+          }
+        case '@' =>
+          if (counter != 0 || isDomain == 1) return None
+          isDomain = 1 - isDomain
+        case _  => if (counter == 0) localAndDomainPart(isDomain).append(c)
+      }
+        index += 1
         if (counter < 0) return None
     }
     Option.when(counter == 0)(localAndDomainPart(0).toString(), localAndDomainPart(1).toString())
