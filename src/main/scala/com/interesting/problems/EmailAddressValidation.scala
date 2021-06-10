3ae25cf6e6463f9e2916b40392a6875151d67815
sauravsahu02 <sauravsahu02@gmail.com>
2021-06-10 13:34:24 +0530

    Removing comments in email-address

---
+++ src/main/scala/com/interesting/problems/EmailAddressValidation.scala
@@ -0,0 +1,28 @@
+package com.interesting.problems
+
+/**
+ * Created by Saurav Sahu on 10th June 2021
+ */
+object EmailAddressValidation {
+  // As poer Wiki-page, comments are allowed with parentheses at either end of the local-part;
+  // e.g., john.smith(comment)@example.com and (comment)john.smith@example.com
+  // are both equivalent to john.smith@example.com.
+  type localAndDomainType = Option[(/*local part*/ String, /*domain part*/ String)]
+  def getLocalPartAndDomain(emailAddress: String): localAndDomainType = {
+    val localAndDomainPart = Array(new StringBuilder, new StringBuilder)
+    var counter = 0
+    var isDomain = 0
+    emailAddress.toCharArray.zipWithIndex.foreach{
+      case (c, idx) =>
+        c match {
+          case '(' => counter += 1
+          case ')' => counter -= 1
+          case '@' =>
+            if (counter != 0 || isDomain == 1) return None
+            isDomain = 1 - isDomain
+          case _  => if (counter == 0) localAndDomainPart(isDomain).append(c)
+        }
+    }
+    Option.when(counter == 0)(localAndDomainPart(0).toString(), localAndDomainPart(1).toString())
+  }
+}
---
+++ src/test/scala/com/interesting/problems/EmailAddressValidationTest.scala
@@ -0,0 +1,64 @@
+package com.interesting.problems
+
+import org.scalatest.FunSuite
+/**
+ * Created by Saurav Sahu on 10th June 2021
+ */
+class EmailAddressValidationTest extends FunSuite {
+  test("local part has comment") {
+    var result = EmailAddressValidation.getLocalPartAndDomain("john.smith(comment)@example.com")
+    assert(result.isDefined)
+    assert(result.get._1 == "john.smith")
+    assert(result.get._2 == "example.com")
+    result = EmailAddressValidation.getLocalPartAndDomain("john.smith(comment)@example.com")
+    assert(result.isDefined)
+    assert(result.get._1 == "john.smith")
+    assert(result.get._2 == "example.com")
+    result = EmailAddressValidation.getLocalPartAndDomain("(comment)john.smith(comment)@example.com")
+    assert(result.isDefined)
+    assert(result.get._1 == "john.smith")
+    assert(result.get._2 == "example.com")
+  }
+  test("domain part has comment") {
+    var result = EmailAddressValidation.getLocalPartAndDomain("john.smith@(comment)example.com")
+    assert(result.isDefined)
+    assert(result.get._1 == "john.smith")
+    assert(result.get._2 == "example.com")
+    result = EmailAddressValidation.getLocalPartAndDomain("john.smith@example.com(comment)")
+    assert(result.isDefined)
+    assert(result.get._1 == "john.smith")
+    assert(result.get._2 == "example.com")
+  }
+  test("both local and domain part has comment") {
+    val result = EmailAddressValidation.getLocalPartAndDomain("john.smith(comment)@example.com")
+    assert(result.isDefined)
+    assert(result.get._1 == "john.smith")
+    assert(result.get._2 == "example.com")
+  }
+  test("both local and domain part has comment with nesting") {
+    val result = EmailAddressValidation.getLocalPartAndDomain(
+      "(anothercomment(comment))john.smith(anothercomment(comment))" +
+        "@" +
+        "(anothercomment(comment))example.com(anothercomment(comment))")
+    assert(result.isDefined)
+    assert(result.get._1 == "john.smith")
+    assert(result.get._2 == "example.com")
+  }
+  test("with extraneous comments in mid") {
+    val result = EmailAddressValidation.getLocalPartAndDomain(
+      "((comment))john.(excludeMe)smith((comment))" +
+        "@" +
+        "((comment))example.com((comment))")
+    assert(result.isDefined)
+    assert(result.get._1 == "john.smith")
+    assert(result.get._2 == "example.com")
+  }
+  test("negative testing") {
+    var result = EmailAddressValidation.getLocalPartAndDomain(
+      "john.smith((comment)@example.com")
+    assert(result.isEmpty)
+    result = EmailAddressValidation.getLocalPartAndDomain(
+      "john.smith@example.com(comment))")
+    assert(result.isEmpty)
+  }
+}
