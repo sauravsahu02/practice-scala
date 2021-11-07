e26d8fa5534adb30cbe06e4827311cf52b367f70
sauravsahu02 <sauravsahu02@gmail.com>
2021-11-07 14:42:59 +0530

    MongoDBExercises

---
+++ src/main/scala/com/persistence/nosql/mongodb/MongoDBExercises.scala
@@ -0,0 +1,16 @@
+/*
+ * Created by Saurav Sahu on 08/07/21, 9:18 PM.
+ */
+
+/*
+ * Created by Saurav Sahu on 08/07/21, 6:37 AM.
+ */
+
+package com.persistence.nosql.mongodb
+
+import org.mongodb.scala.MongoClient
+
+object MongoDBExercises {
+  val mongoClient : MongoClient = MongoClient("mongodb://localhost:27017")
+
+}
---
+++ src/test/scala/com/persistence/nosql/mongodb/MongoDBExercisesTest.scala
@@ -0,0 +1,15 @@
+/*
+ * Created by Saurav Sahu on 08/07/21, 1:41 PM.
+ */
+
+package com.persistence.nosql.mongodb
+
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FunSuite}
+
+class MongoDBExercisesTest extends FunSuite with BeforeAndAfter with BeforeAndAfterAll{
+  val mongoClient = MongoDBExercises.mongoClient
+
+  test("1. Write a MongoDB query to display all the documents in the collection restaurants.") {
+
+  }
+}
